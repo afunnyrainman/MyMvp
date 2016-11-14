d74c669a655d0553b31a0ca0e7010ecccfd6661d
afunnyrainman <876314095@qq.com>
2016-11-14 17:53:51 +0800

    第一次上传

---
+++ app/src/main/java/com/example/user/mvp/mvp/main/BaseView.java
@@ -0,0 +1,12 @@
+package com.example.user.mvp.mvp.main;
+
+/**
+ * Created by Administrator
+ * on 2016/10/19.
+ */
+
+public interface BaseView {
+    void showLoading();
+
+    void hideLoading();
+}
